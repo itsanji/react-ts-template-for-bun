07c95d87d317f6f6f9d0fce279a859695d38b78c
Anji <nhu.nhu.face@gmail.com>
2022-07-09 19:31:06 +0900

    delete manifest. Can get it work T_T.

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <h1>Working</h1>
       </header>
+      <h1>What about this?</h1>
     </div>
   );
 }
