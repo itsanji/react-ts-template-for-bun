a3c657fcb84f5364694215d9da876215b9d3b3c9
Anji <nhu.nhu.face@gmail.com>
2022-07-09 19:31:25 +0900

    app.tsx template

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Working</h1>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
       </header>
       <h1>What about this?</h1>
     </div>
